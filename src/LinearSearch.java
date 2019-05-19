4865a3b31bd0c47454e227387fbfcf9f137c1643
komal.bhatt <komal18@navgurukul.org>
2019-05-19 10:01:22 +0530

    Add.Taking userInput through command line.

---
+++ src/LinearSearch.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int userInput = Integer.parseInt(args[0]);
+        List <Integer> listOfElements = new ArrayList<>(List.of(12,13,15,18,19,10,11));
+    }
+
+}
