d2b243a027289b88ebcd46a2e0ef60ddb3e642be
komal.bhatt <komal18@navgurukul.org>
2019-05-19 10:14:43 +0530

    Add.For checking if number is not present in a list.

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -5,11 +5,15 @@ public class LinearSearch {
     public static void main(String[] args) {
         int userInput = Integer.parseInt(args[0]);
         List <Integer> listOfElements = new ArrayList<>(List.of(12,13,15,18,19,10,11));
+        int count = 0;
         for(int i = 0;i < listOfElements.size();i++){
             if(listOfElements.get(i) == userInput){
                 System.out.println("Index number is "+i);
                 break;
             }
+        }
+        if(count == 0){
+            System.out.println(userInput+" is not present in the list");
         }
     }
 
