64d0ecfe6171c2e33dd8477c1bbc0fd6eb7f6982
komal.bhatt <komal18@navgurukul.org>
2019-05-19 10:07:31 +0530

    Add.For checking index number from the list.

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -5,6 +5,12 @@ public class LinearSearch {
     public static void main(String[] args) {
         int userInput = Integer.parseInt(args[0]);
         List <Integer> listOfElements = new ArrayList<>(List.of(12,13,15,18,19,10,11));
+        for(int i = 0;i < listOfElements.size();i++){
+            if(listOfElements.get(i) == userInput){
+                System.out.println("Index number is "+i);
+                break;
+            }
+        }
     }
 
 }
